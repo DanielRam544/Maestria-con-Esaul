d186f3dd914609333cc2e82201ac424b22523936
Daniel Ramirez <danidehorta544@gmail.com>
2023-11-15 18:34:10 -0600

    Change

--- 13.TabMaterial/App.js
+++ 13.TabMaterial/App.js
@@ -1,6 +1,7 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function HomeScreen() {
   return (
@@ -18,6 +19,14 @@ function UsersScreen() {
   );
 }
 
+function MaterialScreen() {
+  return (
+    <View style={styles.vHome}>
+      <Text style={styles.text}>Material Screen</Text>
+    </View>
+  );
+}
+
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
@@ -26,11 +35,18 @@ export default function App() {
       <Tab.Navigator 
       initialRouteName='Home'
       activeColor='#48A98F'
-      inactiveColor='#906090'
+      inactiveColor='#6C3483'
       barStyle={{backgroundColor: '#85929E'}}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Users" component={UsersScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} 
+        options={{
+          tabBarLabel: 'Home', tabBarIcon: ({color})=> (
+          <MaterialCommunityIcons name='alien' color={color} size={24} />
+        ), }}/>
+        <Tab.Screen name="Users" component={UsersScreen} options=
+        {{tabBarLabel: 'Users', tabBarIcon: ({color})=> (
+          <MaterialCommunityIcons name='account-cowboy-hat' color={color} size={24} />
+        ), }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
